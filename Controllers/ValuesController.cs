9b99d7916331c63d4831438a9f4b340b2e9ab782
Araya Cohen <matias.araya.cohen@accenture.com>
2019-07-04 10:37:35 -0300

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AuthDummyService.Models;
+
+namespace AuthDummyService.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST: login dummy con valores en definidos
+        [HttpPost("/login")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Login(User usuario)
+        {
+            if (usuario.userId == "matias.araya" && usuario.password == "Holamundo") return Ok(usuario);
+            return BadRequest();
+        }        
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthDummyService.Models
+{
+    public class User
+    {
+        public String userId { get; set; }
+        public String password { get; set; }
+    }
+}
